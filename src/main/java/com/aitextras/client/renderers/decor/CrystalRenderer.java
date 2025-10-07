b0216d6318a73c2a6a7418f92356a4557bc36196
Addi3 <122154427+Addi3@users.noreply.github.com>
2025-10-07 23:33:34 +0100

    fix: roof crystal crash? maybe actually?

--- src/main/java/com/aitextras/client/renderers/decor/CrystalRenderer.java
+++ src/main/java/com/aitextras/client/renderers/decor/CrystalRenderer.java
@@ -3,15 +3,12 @@ package com.aitextras.client.renderers.decor;
 import com.aitextras.AITExtras;
 import com.aitextras.client.models.decor.CrystalModel;
 import com.aitextras.core.blockentities.CrystalBlockEntity;
-import dev.amble.ait.core.world.TardisServerWorld;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.RotationAxis;
-import net.minecraft.util.math.RotationPropertyHelper;
 
 public class CrystalRenderer<T extends CrystalBlockEntity> implements BlockEntityRenderer<T> {
 
@@ -26,14 +23,15 @@ public class CrystalRenderer<T extends CrystalBlockEntity> implements BlockEntit
     }
 
     @Override
-    public void render(CrystalBlockEntity entity, float tickDelta, MatrixStack matrices,
-                       VertexConsumerProvider vertexConsumers, int light, int overlay) {
-        if (!entity.isLinked() && TardisServerWorld.isTardisDimension(entity.getWorld())) return;
-        CrystalModel model = new CrystalModel(CrystalModel.getTexturedModelData().createModel());
+    public void render(T entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+
         matrices.push();
-        matrices.scale(1f, 1f, 1f);
+
         matrices.translate(0.5f, 1.5f, 0.5f);
-        matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(180));
+
+        // Retrieve scale dynamically and apply it
+        float scale = entity.getScale();
+        matrices.scale(scale, scale, scale);
 
         this.crystalModel.render(matrices,
                 vertexConsumers.getBuffer(RenderLayer.getEntityTranslucent(CRYSTAL_TEXTURE)), light, overlay, 1.0F,
@@ -42,8 +40,6 @@ public class CrystalRenderer<T extends CrystalBlockEntity> implements BlockEntit
                 vertexConsumers.getBuffer(RenderLayer.getEntityTranslucentEmissive(EMISSIVE_CRYSTAL_TEXTURE)),
                 0xF000F00, overlay, 1.0F, 1.0F, 1.0F, 1.0F);
 
-
         matrices.pop();
-
     }
 }
